d49f44fc938c5dc340103ff1b8677b6dbdbbb0cd
Anton Sozonov <sitehelp.s@gmail.com>
2017-08-16 15:44:56 +0300

    Окружение в конфиге

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isDev = () => NODE_ENV === 'development';
+
 module.exports = {
     entry: "./home",
     output: {
         path: __dirname + "/build",
         filename: "build.js"
     },
-    watch: true,
-    devtool: "eval"  // eval для dev, source-map - для prod
+    watch: isDev(),
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+    
+    devtool: isDev() ? "eval" : "source-map"   // eval для dev, source-map - для prod
 };
